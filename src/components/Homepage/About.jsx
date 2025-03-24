5ed6be965f0fe3473b43e0b0f5292990a10da961
TI Sabbir <tisabbir0@gmail.com>
2025-03-24 19:37:59 +0600

    feat(ui): add about us section

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='flex gap-6 items-center'>
-            <div>
-                <Image src={'/public/assets/images/about_us/person.jpg'} width={400} height={400} alt='about image' />
-                <Image src={'/public/assets/images/about_us/parts.jpg'} width={400} height={400} alt='about image' />
+        <div className='grid grid-cols-1 md:grid-cols-2 justify-center my-24 gap-6 items-center '>
+            <div className='relative'>
+                <Image className='rounded-lg object-cover' src='/assets/images/about_us/person.jpg' width={460} height={480} alt='about image' />
+                <Image className='absolute object-cover right-0 top-48 border-4 border-white rounded-lg' src='/assets/images/about_us/parts.jpg' width={320} height={380} alt='about image' />
             </div>
-            <div>
-                <p>About Us</p>
-                <h1>We are qualified and of experience in the field</h1>
+            <div className='space-y-6'>
+                <p className='text-primary'>About Us</p>
+                <h1 className='text-5xl font-semibold'>We are qualified and of experience in the field</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laudantium asperiores, expedita harum magni libero. Neque provident voluptatibus in blanditiis voluptatem, incidunt, alias sit deleniti molestiae unde amet ipsum porro.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laudantium asperiores, expedita harum magni libero. Neque provident voluptatibus in blanditiis voluptatem, incidunt, alias sit deleniti molestiae unde amet ipsum porro.</p>
-                <button>Get for info</button>
+                <button className='btn btn-primary text-white'>Get for info</button>
             </div>
         </div>
     );
--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import About from './About';
 
 
 const HomePage = () => {
     return (
         <div>
             <Banner />
+            <About />
         </div>
     );
 };
