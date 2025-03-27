c0fdcfcf806da47d8601b99e017c59a34e399cdc
TI Sabbir <tisabbir0@gmail.com>
2025-03-28 05:02:00 +0600

    style(signup): add form submission handle function

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,9 +1,16 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const page = () => {
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    alert("Form Submitted.....")
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4 my-24 items-center">
       <div>
@@ -16,7 +23,7 @@ const page = () => {
       </div>
       <div className="border p-20 rounded-lg text-center space-y-4">
         <h1 className="text-4xl text-center">Sign Up</h1>
-        <form action="">
+        <form onSubmit={handleSubmit} action="">
         <fieldset className="fieldset">
           <legend className="fieldset-legend text-left">Name</legend>
           <input type="text" className="input w-full" placeholder="Your Name" />
@@ -33,7 +40,7 @@ const page = () => {
           <legend className="fieldset-legend text-left">Password</legend>
           <input type="password" className="input  w-full" placeholder="Password" />
         </fieldset>
-        <button className="btn btn-primary text-white w-full mt-6">Sign Up</button>
+        <button type="submit" className="btn btn-primary text-white w-full mt-6">Sign Up</button>
         </form>
         <p>Or Sign Up With</p>
         <div className="flex gap-4 justify-center">
