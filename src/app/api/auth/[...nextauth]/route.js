6599ddd3491b1956b0a58f904ac996caaf728823
TI Sabbir <tisabbir0@gmail.com>
2025-03-28 19:52:38 +0600

    feat(auth): add next auth setup

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth/next";
+import { CredentialsProvider } from "next-auth/providers";
+
+const handler = async () =>
+  NextAuth({
+    session: {
+      strategy: "jwt",
+      maxAge: 30 * 24 * 60 * 60,
+    },
+    providers: [
+      CredentialsProvider({
+        credentials: {
+          email: {},
+          password: {},
+        },
+        async authorize (credentials) {
+            return true;
+        }
+      }),
+    ],
+    callbacks: {},
+    pages: {
+      signIn: "/login",
+    },
+  });
+
+export { handler as GET, handler as POST };
